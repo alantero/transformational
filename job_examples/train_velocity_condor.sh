be6f753b5665e6a58fb01c2304e1a428b1b37acf
alantero <agusem@hotmail.com>
2026-04-14 22:34:41 +0200

    Added condor submit file correct.

---
+++ job_examples/train_velocity_condor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+source /eos/home-a/alantero/t5venv/bin/activate
+
+echo -e "Tu job Condor\nClusterId: ${CLUSTER_ID}\nProcId: ${PROCESS_ID}\nHa comenzado en $(hostname) a las $(date)" \
+  | mail -s "Job ${CLUSTER_ID}.${PROCESS_ID} started" agusem@hotmail.com
+
+nvidia-smi
+
+python /eos/home-a/alantero/transformational/train_velocity.py \
+    --dataset_path "/eos/home-a/alantero/datasets/pretraining_velocity_shards" \
+    --output_dir "/eos/home-a/alantero/transformational/results/velocity-base" \
+    --manifest_dir "/eos/home-a/alantero/transformational/results/velocity-base/manifests" \
+    --d_model 384 \
+    --num_layers 8 \
+    --num_heads 8 \
+    --d_ff 1536 \
+    --max_sequence_length 1024 \
+    --per_device_train_batch_size 128 \
+    --per_device_eval_batch_size 128 \
+    --gradient_accumulation_steps 2 \
+    --num_train_epochs 10 \
+    --learning_rate 3e-4 \
+    --warmup_steps 2000 \
+    --precision fp16 \
+    --num_workers 32 \
+    --logging_steps 500 \
+    --eval_every_steps 2000 \
+    --save_every_steps 2000 \
+    --save_total_limit 3 \
+    --progress_bar always
